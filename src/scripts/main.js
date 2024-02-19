acaea75a4ad037f66083b181da4da5348bc5dfae
kauã gomes <kgs26121967@gmail.com>
2024-02-19 00:04:51 -0300

    commit inicial

---
+++ src/scripts/main.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document
+        .getElementById('form-sorteador')
+        .addEventListener('submit', function(evento) {
+            evento.preventDefault()
+            let numMax = document.getElementById('numero-maximo').value
+            numMax = parseInt(numMax)
+
+            let numRandom = Math.random() * numMax
+            numRandom = Math.floor(numRandom + 1)
+
+            document
+                .getElementById('resultado-valor')
+                .innerText = numRandom
+            document.querySelector('.resultado').style.display = 'block'
+        })
+})
